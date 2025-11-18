1a092c6367f2f6a55df08155da02a9a4575ec8c2
Cyberzilla™ <169317950+FJ-cyberzilla@users.noreply.github.com>
2025-11-18 15:19:35 +0330

    Create __init__.py

---
+++ HyperZilla/ZILLA_CORE/__init__.py
@@ -0,0 +1,10 @@
+#ZILLA_CORE/__init__.py
+"""Core security modules for Hyper-ZiLLA"""
+
+__version__ = "1.0.0"
+__author__ = "Hyper-ZiLLA Team"
+
+# Intelligence modules security imports
+from utils.security import SecurityBridge
+
+__all__ = ['SecurityBridge']
